7dbae2507804646085ab28208dde20f0aab6c468
Atulya <snehitms7@gmail.com>
2025-11-05 13:53:48 +0530

    colorscheme changes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -8,12 +8,12 @@ return {
    require("catppuccin").setup({
      flavour = "mocha", -- latte, frappe, macchiato, mocha
      transparent_background = false,
-     show_end_of_buffer = true, -- Show ~ at end of window to indicate empty lines
+     show_end_of_buffer = false, -- Don't show ~ (matches fillchars config)
      term_colors = true,
      dim_inactive = {
-       enabled = false, -- Dimming inactive windows is disabled
+       enabled = true, -- Dim inactive splits for better focus
        shade = "dark",
-       percentage = 0.15,
+       percentage = 0.10, -- Subtle dimming
      },
      no_italic = false, -- Allow italic styling as defined in 'styles'
      no_bold = false, -- Allow bold styling as defined in 'styles'
@@ -33,10 +33,33 @@ return {
      color_overrides = {},
      custom_highlights = function(colors)
        return {
+         -- Line numbers
          LineNr = { fg = colors.sky },
          NormalFloat = { bg = colors.mantle },
          Search = { bg = colors.lavender, fg = colors.base },
          CursorLineNr = { fg = colors.text, style = { "bold" } },
+         
+         -- CMP ghost text
+         CmpGhostText = { fg = colors.overlay0, italic = true },
+         
+         -- Telescope colors
+         TelescopeNormal = { bg = colors.mantle },
+         TelescopeBorder = { fg = colors.blue, bg = colors.mantle },
+         TelescopePromptNormal = { bg = colors.surface0 },
+         TelescopePromptBorder = { fg = colors.blue, bg = colors.surface0 },
+         TelescopePromptTitle = { fg = colors.base, bg = colors.blue },
+         TelescopePreviewTitle = { fg = colors.base, bg = colors.green },
+         TelescopeResultsTitle = { fg = colors.base, bg = colors.mauve },
+         
+         -- Better diff colors
+         DiffAdd = { bg = colors.green, fg = colors.base },
+         DiffChange = { bg = colors.yellow, fg = colors.base },
+         DiffDelete = { bg = colors.red, fg = colors.base },
+         DiffText = { bg = colors.yellow, fg = colors.base, style = { "bold" } },
+         
+         -- Indent blankline
+         IndentBlanklineChar = { fg = colors.surface0 },
+         IndentBlanklineContextChar = { fg = colors.surface2 },
        }
      end,
      
@@ -46,6 +69,31 @@ return {
        bufferline = true,
        neotree = true,
        treesitter = true,
+       telescope = {
+         enabled = true,
+       },
+       which_key = true,
+       mason = true,
+       noice = true,
+       indent_blankline = {
+         enabled = true,
+         colored_indent_levels = false,
+       },
+       native_lsp = {
+         enabled = true,
+         virtual_text = {
+           errors = { "italic" },
+           hints = { "italic" },
+           warnings = { "italic" },
+           information = { "italic" },
+         },
+         underlines = {
+           errors = { "underline" },
+           hints = { "underline" },
+           warnings = { "underline" },
+           information = { "underline" },
+         },
+       },
      },
    })
    vim.cmd.colorscheme("catppuccin")
