eb105b1a70cf1a85c96d7400acd8c99657401355
Atulya <snehitms7@gmail.com>
2026-01-25 14:41:06 +0530

    chore : fixed a section of options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,16 +1,17 @@
 vim.g.mapleader = " " -- Set leader key to space
-vim.g.maplocalleader = " " -- Set local leader key to space
-vim.keymap.set('i', '<Space>', '<Space>', { noremap = true, silent = true }) -- Prevent space delay in insert mode
 local opt = vim.opt
+
+-- * important never change
 opt.number = true -- Show line numbers
-opt.relativenumber = true -- Show relative line numbers
 opt.tabstop = 2 -- Number of spaces for tab character
 opt.shiftwidth = 2 -- Number of spaces for indentation
 opt.expandtab = true -- Use spaces instead of tabs
-opt.smartindent = true -- Smart autoindenting when starting a new line
+
+-- * regular might change
+opt.relativenumber = true -- Show relative line numbers
 opt.wrap = true -- Enable line wrapping (explicit)
 opt.linebreak = true -- Break lines at word boundaries
-opt.showbreak = "↪ " -- Visual indicator for wrapped lines
+opt.showbreak = "⤷ " -- Visual indicator for wrapped lines
 opt.breakindent = true -- Preserve indentation on wrapped lines
 opt.breakindentopt = "shift:2" -- Additional indent for wrapped lines
 opt.ignorecase = true -- Case insensitive search
@@ -24,17 +25,18 @@ opt.updatetime = 300 -- Faster completion (4000ms default)
 opt.timeoutlen = 300 -- Time to wait for key sequence
 opt.conceallevel = 3 -- Hide markup characters
 opt.concealcursor = "n" -- Hide concealed text in normal mode
+
 opt.pumheight = 10 -- Maximum popup menu height
 opt.showmode = false -- Don't show mode in command line
 opt.shortmess:append("c") -- Don't show completion messages
 opt.whichwrap:append("<>[]hl") -- Allow cursor wrap with arrow keys
 opt.fillchars = {
-  fold = " ",
-  foldopen = "v",
-  foldclose = ">",
-  foldsep = " ",
-  diff = "╱",
-  eob = " ", -- Hide ~ at end of buffer
+	fold = " ",
+	foldopen = "v",
+	foldclose = ">",
+	foldsep = " ",
+	diff = "╱",
+	eob = " ", -- Hide ~ at end of buffer
 }
 opt.scrolloff = 8 -- Keep 8 lines visible when scrolling
 opt.mouse = "" -- Disable mouse support
@@ -49,10 +51,10 @@ opt.undodir = undodir
 
 -- Force 2 spaces for C/C++
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "c", "cpp" },
-  callback = function()
-    vim.opt_local.tabstop = 2
-    vim.opt_local.shiftwidth = 2
-    vim.opt_local.expandtab = true
-  end,
+	pattern = { "c", "cpp" },
+	callback = function()
+		vim.opt_local.tabstop = 2
+		vim.opt_local.shiftwidth = 2
+		vim.opt_local.expandtab = true
+	end,
 })
