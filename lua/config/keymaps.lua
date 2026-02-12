6a6d478ad769d6ae0e8d6f81b4eb120faacb24ba
Atulya <snehitms7@gmail.com>
2026-02-12 12:52:09 +0530

    feat : keymaps .lua commited

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,24 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
--- Custom keymaps for this Neovim configuration
 
 local map = vim.keymap.set
 
 -- Better up/down
 map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
 map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
-
--- i dont really use them for now so commenting them out and not deleting them.
--- Move to window using the <ctrl> hjkl keys
--- map("n", "<C-h>", "<C-w>h", { desc = "Go to left window", remap = true })
--- map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window", remap = true })
--- map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window", remap = true })
--- map("n", "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true })
-
--- Resize window using <ctrl> arrow keys
--- map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
--- map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
--- map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
--- map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
 
 -- Move Lines
 map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
@@ -43,6 +29,3 @@ map("n", "<leader>L", "<cmd>Lazy<cr>", { desc = "Lazy" })
 
 -- new file
 map("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New File" })
-
--- quit
-map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit All" })
