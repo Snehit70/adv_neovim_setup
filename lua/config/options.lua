02518750eff670f5b34f15c1e3ccf15e61aff01e
Atulya <snehitms7@gmail.com>
2026-02-12 12:11:45 +0530

    feat : options.lua updated

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,6 +6,7 @@ opt.number = true -- Show line numbers
 opt.tabstop = 2 -- Number of spaces for tab character
 opt.shiftwidth = 2 -- Number of spaces for indentation
 opt.expandtab = true -- Use spaces instead of tabs
+opt.mouse = "" -- Disable mouse support
 
 -- * regular might change
 opt.relativenumber = true -- Show relative line numbers
@@ -25,22 +26,17 @@ opt.updatetime = 300 -- Faster completion (4000ms default)
 opt.timeoutlen = 300 -- Time to wait for key sequence
 opt.conceallevel = 3 -- Hide markup characters
 opt.concealcursor = "n" -- Hide concealed text in normal mode
-
 opt.pumheight = 10 -- Maximum popup menu height
 opt.showmode = false -- Don't show mode in command line
 opt.shortmess:append("c") -- Don't show completion messages
-opt.whichwrap:append("<>[]hl") -- Allow cursor wrap with arrow keys
 opt.fillchars = {
 	fold = " ",
 	foldopen = "v",
 	foldclose = ">",
 	foldsep = " ",
 	diff = "╱",
-	eob = " ", -- Hide ~ at end of buffer
+	eob = " ",
 }
-opt.scrolloff = 8 -- Keep 8 lines visible when scrolling
-opt.mouse = "" -- Disable mouse support
-opt.clipboard:append("unnamedplus") -- Use system clipboard
 opt.splitright = true -- Split windows to the right (standard behavior)
 opt.splitbelow = true -- Split windows below (standard behavior)
 opt.swapfile = false -- Disable swap files
@@ -48,6 +44,8 @@ opt.undofile = true -- Enable persistent undo
 local undodir = vim.fn.stdpath("data") .. "/undodir"
 vim.fn.mkdir(undodir, "p") -- Create directory if it doesn't exist
 opt.undodir = undodir
+opt.scrolloff = 8 -- Keep 8 lines visible when scrolling
+opt.clipboard:append("unnamedplus") -- Use system clipboard
 
 -- Force 2 spaces for C/C++
 vim.api.nvim_create_autocmd("FileType", {
